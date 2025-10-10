de3014090c1c6c1cbf1ebb870d1a40e06c3f92d3
shehabrakib <shehabprogram128@gmail.com>
2025-10-10 22:56:41 +0600

    Added New Arrivals Section in home page

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/layout/Hero"
 import GenderCollectionSection from "../components/products/GenderCollectionSection"
+import NewArrivals from "../components/products/NewArrivals"
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <GenderCollectionSection/>
+      <NewArrivals/>
     </div>
   )
 }

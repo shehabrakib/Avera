f0361ff1c41e3b9a20c95e5bf43ffef1a8c08381
shehabrakib <shehabprogram128@gmail.com>
2025-10-11 21:11:00 +0600

    Added a new section "Top wear" in home.jsx

--- frontend/components/products/ProductGrid.jsx
+++ frontend/components/products/ProductGrid.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const ProductGrid = ({products}) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
       {products.map((product, index)=>(
         <Link key={index} to={`/product/${product.id}`} className="block">
             <div className="bg-white p-4 rounded-lg">
--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -2,7 +2,65 @@ import Hero from "../components/layout/Hero"
 import GenderCollectionSection from "../components/products/GenderCollectionSection"
 import NewArrivals from "../components/products/NewArrivals"
 import ProductDetails from "../components/products/ProductDetails"
+import ProductGrid from "../components/products/ProductGrid"
+const placeholderProducts = [
+  {
+        _id: 1,
+        name: "Product 1",
+        price: 100,
+        images: [{ url: "https://picsum.photos/500/500?random=1" }],
 
+    },
+    {
+        _id: 2,
+        name: "Product 2",
+        price: 100,
+        images: [{ url: "https://picsum.photos/500/500?random=2" }],
+
+    },
+    {
+        _id: 3,
+        name: "Product 3",
+        price: 100,
+        images: [{ url: "https://picsum.photos/500/500?random=3" }],
+
+    },
+    {
+        _id: 4,
+        name: "Product 4",
+        price: 100,
+        images: [{ url: "https://picsum.photos/500/500?random=4" }],
+
+    },
+    {
+        _id: 5,
+        name: "Product 5",
+        price: 100,
+        images: [{ url: "https://picsum.photos/500/500?random=5" }],
+
+    },
+    {
+        _id: 6,
+        name: "Product 6",
+        price: 100,
+        images: [{ url: "https://picsum.photos/500/500?random=6" }],
+
+    },
+    {
+        _id: 7,
+        name: "Product 7",
+        price: 100,
+        images: [{ url: "https://picsum.photos/500/500?random=7" }],
+
+    },
+    {
+        _id: 8,
+        name: "Product 8",
+        price: 100,
+        images: [{ url: "https://picsum.photos/500/500?random=8" }],
+
+    },
+]
 const Home = () => {
   return (
     <div>
@@ -13,6 +71,12 @@ const Home = () => {
         Best Seller
       </h2>
       <ProductDetails/>
+      <div className="container mx-auto">
+        <h2 className="text-3xl text-center font-bold mb-4">
+          Top Wears for Women
+        </h2>
+        <ProductGrid products={placeholderProducts}/>
+      </div>
     </div>
   )
 }

7cd0a638144eff2e7909bce1ab32bccb1884e661
shehabrakib <shehabprogram128@gmail.com>
2025-10-18 10:46:54 +0600

    Completed Checkout page design except stripe

--- frontend/components/cart/Checkout.jsx
+++ frontend/components/cart/Checkout.jsx
@@ -174,6 +174,44 @@ const Checkout = () => {
                     </div>
                 </form>
             </div>
+            {/* right section  */}
+            <div className="bg-gray-50 p-6 rounded-lg">
+                <h3 className="text-lg mb-4">Order Summary</h3>
+                <div className="border-t py-4 mb-4">
+                    {cart.products.map((product,index)=>(
+                        <div key={index}
+                        className="flex items-start justify-between py-2 border-b"
+                        >
+                            <div className="flex items-start">
+                                <img
+                                src={product.image}
+                                alt={product.name}
+                                className="w-20 h-24 object-cover mr-4"
+                                />
+                                <div>
+                                    <h3 className="text-md">{product.name}</h3>
+                                    <p className="text-gray-500">{product.size}</p>
+                                    <p className="text-gray-500">{product.color}</p>
+                                </div>
+
+                            </div>
+                            <p className="text-xl">${product.price?.toLocaleString()}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="flex justify-between items-center text-lg mb-4">
+                    <p>Subtotal</p>
+                    <p>${cart.totalPrice?.toLocaleString()}</p>
+                </div>
+                <div className="flex justify-between items-center text-lg">
+                    <p>Shipping</p>
+                    <p>Free</p>
+                </div>
+                <div className="flex justify-between items-center text-lg mt-4 border-t pt-4">
+                    <p>Total</p>
+                    <p>${cart.totalPrice?.toLocaleString()}</p>
+                </div>
+            </div>
         </div>
     )
 }

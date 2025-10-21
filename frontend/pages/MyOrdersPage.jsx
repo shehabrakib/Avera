e6107666b7ea6b0e42317952fa99dc4778ba5cf1
shehabrakib <shehabprogram128@gmail.com>
2025-10-21 10:40:25 +0600

    Completed orderdetailed page ui and fixed linked

--- frontend/pages/MyOrdersPage.jsx
+++ frontend/pages/MyOrdersPage.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const MyOrdersPage = () => {
     const [orders, setOrders] = useState([])
+    const navigate = useNavigate()
 
     useEffect(() => {
         setTimeout(() => {
@@ -38,6 +40,10 @@ const MyOrdersPage = () => {
             setOrders(mockOrders)
         }, 1000)
     }, [])
+    const handleRowClick = (orderId) =>{
+        navigate(`/order/${orderId}`)
+    }
+
     return (
         <div className="max-w-7xl mx-auto p-4 sm:p-6">
             <h2 className="text-xl sm:text-2xl font-bold mb-6">My Orders</h2>
@@ -56,7 +62,9 @@ const MyOrdersPage = () => {
                     </thead>
                     <tbody>
                         {orders.length > 0 ? (orders.map((order) => (
-                            <tr key={order._id} className="border-b hover:border-gray-50 cursor-pointer">
+                            <tr key={order._id} 
+                            onClick={()=>handleRowClick(order._id)}
+                            className="border-b hover:border-gray-50 cursor-pointer">
                                 <td className="py-2 px-2 sm:py-4 sm:px-4">
                                     <img src={order.orderItems[0].image} alt={order.orderItems[0].name}
                                         className="w-10 h-10 sm:w-12 sm:h-12 object-cover rounded-lg"

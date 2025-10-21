43298b1c33c5a6e86f1ab9924532a63f90339669
shehabrakib <shehabprogram128@gmail.com>
2025-10-21 21:42:23 +0600

    Completed the usermanagement page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import OrderDetailsPage from '../pages/OrderDetailsPage'
 import MyOrdersPage from '../pages/MyOrdersPage'
 import AdminLayout from '../components/admin/AdminLayout'
 import AdminHomePage from '../pages/AdminHomePage'
+import UserManagement from '../components/admin/UserManagement'
 
 const App = () => {
   return (
@@ -33,6 +34,7 @@ const App = () => {
         </Route>
         <Route path='/admin' element={<AdminLayout/>}>
           <Route index element={<AdminHomePage/>}/>
+          <Route path='users' element={<UserManagement/>}/>
         </Route>
       </Routes>
     </BrowserRouter>

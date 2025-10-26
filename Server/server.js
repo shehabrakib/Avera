902b84677182deda8ad9db9a6d137c6336cac0e6
shehabrakib <shehabprogram128@gmail.com>
2025-10-26 11:22:20 +0600

    succesfully connected database

--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-dotenv.config();
-
-// console.log(process.env.PORT);
+import connectDB from "./config/db.js";
 
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
+
+
+//connecting to mongdb
+connectDB();
 
 app.get("/", (req,res) =>{
     res.send("Welcome to api");

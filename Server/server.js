d6d297d2f335ceb0bae7b1a1831e815609824392
shehabrakib <shehabprogram128@gmail.com>
2025-10-26 11:13:01 +0600

    created git ignore and connected to mongodb

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// console.log(process.env.PORT);
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 9000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res) =>{
     res.send("Welcome to api");

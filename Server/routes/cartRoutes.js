3de4ce6b6662a751b0db17424968f1242b9dfe8b
shehabrakib <shehabprogram128@gmail.com>
2025-10-30 11:04:39 +0600

    created post request for cart routes

---
+++ Server/routes/cartRoutes.js
@@ -0,0 +1,82 @@
+import express from "express";
+import Cart from "../models/Cart.js";
+import Product from "../models/Products.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+const getCart = async (userId, guestId) => {
+  if (userId) {
+    return await Cart.findOne({ user: userId });
+  } else if (guestId) {
+    return await Cart.findOne({ guestId });
+  }
+  return null;
+};
+
+//post /api/cart
+router.post("/", async (req, res) => {
+  const { productId, quantity, size, color, guestId, userId } = req.body;
+  try {
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    // determine guest or legit user
+    let cart = await getCart(userId, guestId);
+
+    if (cart) {
+      const productIndex = cart.products.findIndex(
+        (p) =>
+          p.productId.toString() === productId &&
+          p.size === size &&
+          p.color === color
+      );
+
+      if (productIndex > -1) {
+        cart.products[productIndex].quantity += quantity;
+      } else {
+        cart.products.push({
+          productId,
+          name: product.name,
+          image: product.images[0].url,
+          price: product.price,
+          size,
+          color,
+          quantity,
+        });
+      }
+
+      //recalculate the total price
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0
+      );
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      const newCart = await Cart.create({
+        user: userId ? userId : undefined,
+        guestId: guestId ? guestId : "guest_" + new Date().getTime(),
+        products: [
+          {
+            productId,
+            name: product.name,
+            image: product.images[0].url,
+            price: product.price,
+            size,
+            color,
+            quantity,
+          },
+        ],
+        totalPrice: product.price * quantity,
+      });
+      return res.status(201).json(newCart);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+export default router;
--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -25,6 +26,7 @@ app.get("/", (req,res) =>{
 //API 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
 
 
 app.listen(PORT, ()=>{

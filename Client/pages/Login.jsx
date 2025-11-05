e81b6dd54b4dd81f1a8fcbc1944d453578105f5e
shehabrakib <shehabprogram128@gmail.com>
2025-11-05 11:00:27 +0600

    connected redux store to login page

--- Client/pages/Login.jsx
+++ Client/pages/Login.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
 import login from "../src/assets/login.webp"
-
+import { loginUser } from "../src/redux/slices/authSlice"
+import { useDispatch } from "react-redux"
 
 const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const dispatch = useDispatch()
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log("User Registered:", {email, password})
+        dispatch(loginUser({email,password}))
     }
     return (
         <div className="flex">
--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -17,33 +17,37 @@ import UserManagement from '../components/admin/UserManagement'
 import ProductManagement from '../components/admin/ProductManagement'
 import EditProductPage from '../components/admin/EditProductPage'
 import OrderManagement from '../components/admin/OrderManagement'
+import {Provider} from 'react-redux'
+import store from './redux/store'
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Toaster position='top-right' />
-      <Routes>
-        <Route path='/' element={<UserLayout/>}>
-          <Route index element={<Home/>}/>
-          <Route path='login' element={<Login/>}/>
-          <Route path='register' element={<Register/>}/>
-          <Route path='profile' element={<Profile/>}/>
-          <Route path='collections/:collection' element={ <CollectionPage/>}/>
-          <Route path='product/:id' element={<ProductDetails/>}/>
-          <Route path='checkout' element={<Checkout/>}/>
-          <Route path='order-confirmation' element={<OrderConfirmationPage/>}/>
-          <Route path='order/:id' element={<OrderDetailsPage/>}/>
-          <Route path='my-orders' element={<MyOrdersPage/>}/>
-        </Route>
-        <Route path='/admin' element={<AdminLayout/>}>
-          <Route index element={<AdminHomePage/>}/>
-          <Route path='users' element={<UserManagement/>}/>
-          <Route path='products' element={<ProductManagement/>}/>
-          <Route path='products/:id/edit' element={<EditProductPage/>}/>
-          <Route path='orders' element={<OrderManagement/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+      <Toaster position='top-right' />
+        <Routes>
+          <Route path='/' element={<UserLayout/>}>
+            <Route index element={<Home/>}/>
+            <Route path='login' element={<Login/>}/>
+            <Route path='register' element={<Register/>}/>
+            <Route path='profile' element={<Profile/>}/>
+            <Route path='collections/:collection' element={ <CollectionPage/>}/>
+            <Route path='product/:id' element={<ProductDetails/>}/>
+            <Route path='checkout' element={<Checkout/>}/>
+            <Route path='order-confirmation' element={<OrderConfirmationPage/>}/>
+            <Route path='order/:id' element={<OrderDetailsPage/>}/>
+            <Route path='my-orders' element={<MyOrdersPage/>}/>
+          </Route>
+          <Route path='/admin' element={<AdminLayout/>}>
+            <Route index element={<AdminHomePage/>}/>
+            <Route path='users' element={<UserManagement/>}/>
+            <Route path='products' element={<ProductManagement/>}/>
+            <Route path='products/:id/edit' element={<EditProductPage/>}/>
+            <Route path='orders' element={<OrderManagement/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 

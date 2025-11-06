010bdeb46e799710b9a17938d4a508e3500b700d
shehabrakib <shehabprogram128@gmail.com>
2025-11-06 21:08:51 +0600

    created adminOrderSlice

---
+++ Client/src/redux/slices/adminOrderSlice.js
@@ -0,0 +1,129 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios"
+
+//fetch all orders (admin only)
+export const fetchAllOrders = createAsyncThunk(
+    "adminOrders/fetchAllOrders",
+    async (_, {rejectWithValue}) => {
+        try {
+            const response = await axios.get(
+                `${import.meta.env.VITE_BACKEND_URL}/api/admin/orders`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+                    },
+                }
+            )
+            return response.data
+        } catch (error) {
+            console.error(error)
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
+
+//update order status (admin only)
+
+export const updateOrderStatus = createAsyncThunk(
+    "adminOrders/updateOrderStatus",
+    async ({id, status}, {rejectWithValue}) => {
+        try {
+            const response = await axios.put(
+                `${import.meta.env.VITE_BACKEND_URL}/api/admin/orders/${id}`,
+                {status},
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+                    },
+                }
+            )
+            return response.data
+        } catch (error) {
+            console.error(error)
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
+//Delete and order (admin only)
+export const deleteOrder = createAsyncThunk(
+    "adminOrders/deleteOrder",
+    async (id, {rejectWithValue}) => {
+        try {
+            await axios.delete(
+                `${import.meta.env.VITE_BACKEND_URL}/api/admin/orders/${id}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+                    },
+                }
+            )
+            return id
+        } catch (error) {
+            console.error(error)
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
+
+const adminOrderSlice = createSlice({
+    name: "adminOrders",
+    initialState: {
+        orders: [],
+        totalOrders:0,
+        totalSales:0,
+        loading: false,
+        error: null,
+    },  
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            //fetch all orders
+            .addCase(fetchAllOrders.pending, (state) => {
+                state.loading = true    
+                state.error = null
+            }   )
+            .addCase(fetchAllOrders.fulfilled, (state, action) => {
+                state.loading = false
+                state.orders = action.payload
+                state.totalOrders = action.payload.length
+                const totalSales = action.payload.reduce((total, order) => total + order.totalPrice, 0);
+                state.totalSales = totalSales
+            })
+            .addCase(fetchAllOrders.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload.message || "Could not fetch orders"
+            })  
+            //update order status
+            .addCase(updateOrderStatus.pending, (state) => {
+                state.loading = true
+                state.error = null
+            })
+            .addCase(updateOrderStatus.fulfilled, (state, action) => {
+                state.loading = false
+                const index = state.orders.findIndex(order => order._id === action.payload._id) 
+                if (index !== -1) {
+                    state.orders[index] = action.payload
+                }
+            })
+            .addCase(updateOrderStatus.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload.message || "Could not update order status"
+            })
+            //delete order
+            .addCase(deleteOrder.pending, (state) => {
+                state.loading = true
+                state.error = null
+            })
+            .addCase(deleteOrder.fulfilled, (state, action) => {
+                state.loading = false
+                state.orders = state.orders.filter(order => order._id !== action.payload)   
+            })
+            .addCase(deleteOrder.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload.message || "Could not delete order"
+            })
+    }
+})
+
+export default adminOrderSlice.reducer
+
--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -6,8 +6,7 @@ import checkoutReducer from "./slices/checkoutSlice"
 import orderReducer from "./slices/orderSlice"
 import adminReducer from "./slices/adminSlice"
 import adminProductReducer from "./slices/adminProductSlice"
-
-
+import adminOrderReducer from "./slices/adminOrderSlice"
 const store = configureStore({
     reducer: {
         auth : authReducer,
@@ -17,6 +16,7 @@ const store = configureStore({
         orders: orderReducer,
         admin: adminReducer,
         adminProducts: adminProductReducer,
+        adminOrders: adminOrderReducer,
     },
 });
 

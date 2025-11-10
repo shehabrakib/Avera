7b50523c2108f3a25860899a16596902cf3c1258
shehabrakib <shehabprogram128@gmail.com>
2025-11-10 12:05:49 +0600

    connected OrdersDetailsPage to store and fixed checkout schema fields

--- Client/pages/OrderDetailsPage.jsx
+++ Client/pages/OrderDetailsPage.jsx
@@ -1,39 +1,54 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { Link, useParams } from "react-router-dom"
+import { fetchOrderDetails } from "../src/redux/slices/orderSlice"
 
 const OrderDetailsPage = () => {
     const { id } = useParams()
-    const [orderDetails, setOrderDetails] = useState(null)
+    const dispatch = useDispatch()
+    const  {orderDetails, loading, error} = useSelector((state)=>state.orders)
+    useEffect(()=>{
+        dispatch(fetchOrderDetails(id))
+    },[dispatch,id])
 
-    useEffect(() => {
-        const mockOrderDetails = {
-            _id: id,
-            createdAt: new Date(),
-            isPaid: false,
-            isDelivered: false,
-            paymentMethod: "stripe",
-            shippingMethod: "Standard",
-            shippingAddress: { city: "New York", country: "USA" },
-            orderItems: [
-                {
-                    productId: "1",
-                    name: "Jacket",
-                    price: 120,
-                    quantity: 1,
-                    image: "https://picsum.photos/150?random=1",
-                },
-                {
-                    productId: "2",
-                    name: "T-Shirt",
-                    price: 120,
-                    quantity: 1,
-                    image: "https://picsum.photos/150?random=2",
-                },
+    //const [orderDetails, setOrderDetails] = useState(null)
 
-            ]
-        }
-        setOrderDetails(mockOrderDetails)
-    }, [id])
+    // useEffect(() => {
+    //     const mockOrderDetails = {
+    //         _id: id,
+    //         createdAt: new Date(),
+    //         isPaid: false,
+    //         isDelivered: false,
+    //         paymentMethod: "stripe",
+    //         shippingMethod: "Standard",
+    //         shippingAddress: { city: "New York", country: "USA" },
+    //         orderItems: [
+    //             {
+    //                 productId: "1",
+    //                 name: "Jacket",
+    //                 price: 120,
+    //                 quantity: 1,
+    //                 image: "https://picsum.photos/150?random=1",
+    //             },
+    //             {
+    //                 productId: "2",
+    //                 name: "T-Shirt",
+    //                 price: 120,
+    //                 quantity: 1,
+    //                 image: "https://picsum.photos/150?random=2",
+    //             },
+
+    //         ]
+    //     }
+    //     setOrderDetails(mockOrderDetails)
+    // }, [id])
+
+    if(loading){
+        return <p>Loading...</p>
+    }
+    if(error){
+        return <p>Error; {error}</p>
+    }
 
     return (
         <div className="max-w-7xl mx-auto p-4 sm:p-6">
@@ -111,7 +126,7 @@ const OrderDetailsPage = () => {
                                                 </Link>
                                             </td>
                                             <td className="py-2 px-4">${item.price}</td>
-                                            <td className="py-2 px-4">${item.quantity}</td>
+                                            <td className="py-2 px-4">{item.quantity}</td>
                                             <td className="py-2 px-4">${item.price * item.quantity}</td>
                                         </tr>
                                     ))}

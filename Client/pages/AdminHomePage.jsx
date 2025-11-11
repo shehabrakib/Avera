7b82e139c078b10d13b814ba5e3a7b6efd644bae
shehabrakib <shehabprogram128@gmail.com>
2025-11-11 10:34:06 +0600

    conected admihomepage to store

--- Client/pages/AdminHomePage.jsx
+++ Client/pages/AdminHomePage.jsx
@@ -1,55 +1,89 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import { fetchAdminProducts } from "../src/redux/slices/adminProductSlice"
+import { fetchAllOrders } from "../src/redux/slices/adminOrderSlice"
 
 const AdminHomePage = () => {
-    const orders = [
-        {
-            _id: 123123,
-            user: {
-                name: "John doe",
-            },
-            totalPrice: 110,
-            status: "Processing",
-        },
-        {
-            _id: 123123,
-            user: {
-                name: "John doe",
-            },
-            totalPrice: 110,
-            status: "Processing",
-        },
-        {
-            _id: 123123,
-            user: {
-                name: "John doe",
-            },
-            totalPrice: 110,
-            status: "Processing",
-        },
-    ]
+    // const orders = [
+    //     {
+    //         _id: 123123,
+    //         user: {
+    //             name: "John doe",
+    //         },
+    //         totalPrice: 110,
+    //         status: "Processing",
+    //     },
+    //     {
+    //         _id: 123123,
+    //         user: {
+    //             name: "John doe",
+    //         },
+    //         totalPrice: 110,
+    //         status: "Processing",
+    //     },
+    //     {
+    //         _id: 123123,
+    //         user: {
+    //             name: "John doe",
+    //         },
+    //         totalPrice: 110,
+    //         status: "Processing",
+    //     },
+    // ]
+    const dispatch = useDispatch()
+    const{
+        products,
+        loading: productsLoading,
+        error: productError,
+    } = useSelector((state) => state.adminProducts)
+
+    const {
+        orders,
+        totalOrders,
+        totalSales,
+        loading: ordersLoading,
+        error: ordersError,
+    } = useSelector((state) => state.adminOrders)
+
+
+    useEffect(()=>{
+        dispatch(fetchAdminProducts())
+        dispatch(fetchAllOrders())
+    },[dispatch])
     return (
         <div className="max-w-7xl mx-auto p-6">
             <h1 className="text-3xl font-bold mb-6">Admin Dashboard</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {productsLoading || ordersLoading ? (
+                <p>Loading...</p>
+            )
+            : productError ? (
+                <p className="text-red-500">Error fetching products: {productError}</p>
+            ): ordersError ? (
+                <p className="text-red-500">Error fetching orders: {ordersError}</p>
+            ):(
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 <div className="p-4 shadow-md rounded-lg">
                     <h2 className="text-xl font-semibold">Revenue</h2>
-                    <p className="text-2xl">$10000</p>
+                    <p className="text-2xl">${totalSales.toFixed(2)}</p>
                 </div>
                 <div className="p-4 shadow-md rounded-lg">
                     <h2 className="text-2xl font-semibold">Total Orders</h2>
-                    <p className="text-2xl">200</p>
+                    <p className="text-2xl">{totalOrders}</p>
                     <Link to="/admin/orders" className="text-blue-500 hover:underline">
                         Manage Orders
                     </Link>
                 </div>
                 <div className="p-4 shadow-md rounded-lg">
                     <h2 className="text-2xl font-semibold">Total Products</h2>
-                    <p className="text-2xl">100</p>
+                    <p className="text-2xl">{products?.length}</p>
                     <Link to="/admin/products" className="text-blue-500 hover:underline">
                         Manage Products
                     </Link>
                 </div>
-            </div>
+                </div>
+            )}
+            
             <div className="mt-6">
                 <h2 className="text-2xl font-bold mb-4">Recent Orders</h2>
                 <div className="overflow-x-auto">
@@ -67,11 +101,11 @@ const AdminHomePage = () => {
 
                                 orders.map((order) => (
                                     <tr
-                                        key={order.id}
+                                        key={order._id}
                                         className="border-b hover:bg-gray-50 cursor-pointer">
                                         <td className="p-4">#{order._id}</td>
                                         <td className="p-4">{order.user.name}</td>
-                                        <td className="p-4">{order.totalPrice}</td>
+                                        <td className="p-4">{order.totalPrice.toFixed(2)}</td>
                                         <td className="p-4">{order.status}</td>
 
                                     </tr>
--- Client/src/redux/slices/adminProductSlice.js
+++ Client/src/redux/slices/adminProductSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
 
-const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api/admin/products`
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}`
 const USER_TOKEN = `Bearer ${localStorage.getItem("userToken")}`
 
 //async thunk too fetch products (admin only)

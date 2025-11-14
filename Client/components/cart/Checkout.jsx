2f2249f0306aa85f224b46dde972ee20a3c0e9dd
shehabrakib <shehabprogram128@gmail.com>
2025-11-14 20:04:03 +0600

    fixed file name for payment button in checkout

--- Client/components/cart/Checkout.jsx
+++ Client/components/cart/Checkout.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-import StripeButton from "./stripebutton"
+import StripeButton from "./Stripebutton"
 import { useDispatch, useSelector } from "react-redux"
 import { createCheckoutSession } from "../../src/redux/slices/checkoutSlice"
 import axios from "axios"
